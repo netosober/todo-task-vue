1208ad49321f0177fc0697945ccfaa37cd8ebbc8
netosober <netosober@gmail.com>
2018-07-16 10:51:55 -0700

    Remove stock component and add todo task basic functionality

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,68 @@
 <template>
-  <div id="app">
+  <div id="app" class="container">
     <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>ToDo tasks</h1>
+    <ul class="list-group">
+      <li class="list-group-item" v-for="task in tasks" :key="task.id">
+        <div class="task">
+          <div class="title">{{task.title}}</div>
+          <span class="badge badge-light">{{task.createdDate}}</span>
+        </div>
+      </li>
+      <li class="list-group-item" v-if="addingTask">
+        <form v-on:submit.prevent>
+          <div class="form-group">
+            <label for="taskTitle">New task title</label>
+            <input id="taskTitle" class="form-control" v-model="newTaskTitle" >
+          </div>
+          <div class="form-group">
+            <button type="button" class="btn btn-secondary" @click="cancelNewTask">Cancel</button>
+            <button type="button" class="btn btn-primary" @click="saveNewTask">Save</button>
+          </div>
+        </form>
+      </li>
+      <li class="list-group-item" v-if="!addingTask" >
+        <button type="button" class="btn btn-primary" @click="showNewTask">New task</button>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
+  data: function() {
+    return {
+      counter: 0,
+      tasks: [
+      ],
+      newTaskTitle:"",
+      addingTask: false
+    }
+  },
+  methods: {
+    showNewTask: function() {
+      this.addingTask = true;
+    },
+    cancelNewTask: function() {
+      this.addingTask = false;
+      this.newTaskTitle = "";
+    },
+    saveNewTask: function() {
+      this.tasks.push( {
+        id: this.counter,
+        title: this.newTaskTitle,
+        createdDate: new Date()
+      });
+      this.counter++;
+      this.cancelNewTask();
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
